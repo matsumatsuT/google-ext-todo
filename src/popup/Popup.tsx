8cc3243a2fec40c7ee7247c3bdfb623cefe75206
matsumoto <dsongben@gmail.com>
2023-12-29 12:02:51 +0900

    emotion導入

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 export const Popup = () => {
   return (
-    <div>
+    <div css={wrapperStyle}>
       <h1>Popup Page</h1>
+      <div css={{ color: "pink" }}>テストです</div>
     </div>
   );
 };
+
+const wrapperStyle = css({
+  width: "100%",
+  color: "red",
+});
